fcf60de25786bef4841cd0f7694f5d3ef468f561
İbrahim Enes <49075207+EnesDgirmenci@users.noreply.github.com>
2021-02-14 21:54:20 +0300

    Add files via upload

---
+++ ClassMetotDemo/ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Adi+" isimli müşteri eklendi");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " isimli müşteri Listelendi");
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " isimli müşteri'nin kaydı silinmiştir");
+        }
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.TelNo = 05346892766;
+            musteri1.Adi = "Mehmet";
+            musteri1.Soyadi = "Bakmaz";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.TelNo = 05058647735;
+            musteri2.Adi = "Osman";
+            musteri2.Soyadi = "Durur";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.TelNo = 05062493765;
+            musteri3.Adi = "Ahmet";
+            musteri3.Soyadi = "Şaşkın";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.TelNo = 05326489973;
+            musteri4.Adi = "Hilal";
+            musteri4.Soyadi = "Yılmaz";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4 };
+
+            //müşteri adları kontrol
+            //foreach (Musteri musteri in musteriler)
+            //{
+            //    Console.WriteLine(musteri.Adi);
+            //    Console.WriteLine(musteri.Soyadi);
+            //    Console.WriteLine(musteri.TelNo);
+            //    Console.WriteLine("------");
+            //}
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Ekle(musteri4);
+            
+            Console.WriteLine("--------------------------------");
+
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+            musteriManager.Listele(musteri4);
+
+            Console.WriteLine("--------------------------------");
+
+            musteriManager.Silme(musteri1);
+            musteriManager.Silme(musteri2);
+            musteriManager.Silme(musteri3);
+            musteriManager.Silme(musteri4);
+
+            Console.WriteLine("--------------------------------");
+
+        }
+    }
+}
